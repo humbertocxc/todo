d0a036989f7d42fa53742f540ee3e0ac05f7c727
humbertocxc <humberto.gnm98@gmail.com>
2021-09-16 19:36:27 -0300

    TaskList atualizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,17 +14,29 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+  function handleCreateNewTask() {                    //aqui chamo a função setTasks para a nova lista ser criada
+    setTasks([...tasks, {                             //ela recebe a listagem dos itens já existentes na lista tasks
+      id: tasks.length,                               //e um novo objeto {} do tipo Task, que tem como id o tamanho
+      title: newTaskTitle,                            //da lista para não ocorrerem repetições de ids, o nome contido
+      isComplete: false,                              //no state newTaskTitle e o isComplete começando como falso
+    }])
   }
 
-  function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+  function handleToggleTaskCompletion(id: number) {   //para a atualização de status do isComplete, a função
+    setTasks(                                         //setTasks é chamada, onde dentro dela é feito
+      tasks.map((tasks) => tasks.id === id? {         //um map da lista tasks, onde para cada item contido
+        id: id,                                       //na lista é feito uma comparação de seu id com o 
+        title: tasks.title,                           //id recebido como parâmetro, de modo que quando os valores
+        isComplete: !tasks.isComplete,                //são diferentes o item é retornado normalmente e quando
+      } : tasks)                                      //são iguais, um novo objeto é criado como valor do isComplete
+    )                                                 //sendo o oposto do anterior, e esse novo objeto é retornado
   }
 
-  function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
-  }
+  function handleRemoveTask(id: number) {             //para a remoção de uma task, a função setTasks é chamada
+    setTasks(                                         //onde dentro dela é feito um filter da lista tasks
+      tasks.filter((tasks) => tasks.id !== id)        //onde são mantidos os itens cujas ids são diferentes
+    )                                                 //da id recebida como argumento da função. Como as ids
+  }                                                   //são todas diferentes, apenas um item é removido
 
   return (
     <section className="task-list container">
